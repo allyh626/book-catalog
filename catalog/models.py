49b46e67a01a36a26a741e8490aa3cc8e8a4b700
Ally Hernandez <ashernandez3@buffs.wtamu.edu>
2026-09-20 01:10:51 -0500

    Defined Three Models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    in_print = models.BooleanField(default=True)
+    publisher = models.ForeignKey(
+        Publisher, on_delete=models.CASCADE)
+
+    def __str__(self):
+            return self.title
+
+class Review(models.Model):
+    body = models.TextField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body
